69b0ca1086a6e4ef1e116af40fbd31838277cf7f
maryam ahmed <maryanartan6@gmail.com>
2025-07-27 15:35:42 +0200

    i have added an empty fragment and h2 line

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,8 +1,15 @@
 // Greeting.js
-import React from "react";
 
+import React, { Fragment } from "react";
 function Greeting(props) {
-  return <h1 className="text-pink-500">Hello, {props.name} {props.surname}!</h1>;
+  return (
+    <>
+      <h1 className="text-pink-500">
+        Hello, {props.name} {props.surname}!
+      </h1>
+      <h2>this text was not there</h2>
+    </>
+  );
 }
 
 export default Greeting;
